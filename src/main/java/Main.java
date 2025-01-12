150144822992e5e25621900395f47c93aecea61c
MarceloDarwin <mpenao@est.ups.edu.ec>
2025-01-12 16:59:37 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+package vista;
+
+public class Main {
+  public static void main(String[] args) {
+    ConsolaVista vista = new ConsolaVista();
+    vista.iniciar();
+  }
+}
---
+++ src/main/java/modelo/estructurasdedatos/Colas.java
@@ -0,0 +1,2 @@
+package modelo.estructurasdedatos;public class Colas {
+}
